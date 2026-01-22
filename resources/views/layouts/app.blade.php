6e1c2a7426332c2ca4ab37cb1b1fffb85342fcbe
ohsofreshcreative <132752926+ohsofreshcreative@users.noreply.github.com>
2026-01-22 14:40:40 +0100

    Automatyczny deploy

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,6 +2,18 @@
 <html @php(language_attributes())>
 
 <head>
+	<!-- Google tag (gtag.js) -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=AW-17896034766"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+		gtag('js', new Date());
+		gtag('config', 'AW-17896034766');
+	</script>
+
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	@php(do_action('get_header'))
